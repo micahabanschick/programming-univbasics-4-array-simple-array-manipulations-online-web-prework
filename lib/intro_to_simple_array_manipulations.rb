42551ec402a014698fe7cd88077ac43451c43f92
Micah Banschick <micha.ban@gmail.com>
2020-01-13 19:07:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,12 +18,12 @@ def using_unshift(array,string)
   array.unshift(string)
 end 
 
-def using_unshift(array,string)
-  array.unshift(string)
+def using_pop(array)
+  array.pop()
 end 
 
-def using_unshift(array,string)
-  array.unshift(string)
+def pop_with_args(array)
+  array.pop(2)
 end 
 
 def using_unshift(array,string)
