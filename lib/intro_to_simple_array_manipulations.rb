9227b0ec095713da558f1dd7b81469f213a5fee7
Micah Banschick <micha.ban@gmail.com>
2020-01-13 19:03:14 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(Array,String)
+  Array.push(String)
+end 
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end 
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
+
+def using_unshift(Array,String)
+  Array.unshift(String)
+end
